7009ee595e53ed61c4bbcd6ef3360f787d75dd1f
Andrew Thompson <andrewkt@aktzero.com>
2013-10-18 23:27:18 -0400

    Add and Remove elements from Panel & StudentRecords()

--- GradeBookTest/Form1.vb
+++ GradeBookTest/Form1.vb
@@ -13,4 +13,26 @@
         Me.flpWorkSheet.PerformLayout()
     End Sub
 
+    Private Sub nudStudentCount_ValueChanged(sender As Object, e As EventArgs) Handles nudStudentCount.ValueChanged
+        If IsNothing(nudStudentCount) Or IsNothing(StudentRecords) Then
+            Return
+        End If
+        Me.flpWorkSheet.SuspendLayout()
+        Dim newCount As Integer = nudStudentCount.Value
+        Dim oldCount As Integer = StudentRecords.GetUpperBound(0)
+        If newCount > oldCount Then
+            ReDim Preserve StudentRecords(newCount)
+            For x = oldCount To newCount - 1
+                StudentRecords(x) = New StudentRecord(5)
+                Me.flpWorkSheet.Controls.Add(StudentRecords(x).Row)
+            Next
+        ElseIf newCount < oldCount Then
+            For x = oldCount - 1 To newCount Step -1
+                Me.flpWorkSheet.Controls.RemoveAt(x)
+            Next
+            ReDim Preserve StudentRecords(newCount)
+        End If
+        Me.flpWorkSheet.ResumeLayout()
+
+    End Sub
 End Class
